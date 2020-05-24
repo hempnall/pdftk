89949b7defb720367d15bf5f55849511ff531c8e
James <james@Jamess-Mac-mini.broadband>
2020-05-24 13:07:59 +0100

    changed logging in main.sh

--- pdftk/main.sh
+++ pdftk/main.sh
@@ -1,4 +1,20 @@
 #! /usr/bin/env bash
+show_usage() {
+    echo "Usage: $0 [rotate|interleave|pagecount] [options]"
+    echo "  rotate [90|180|270|360] [filename] [output_filename]"
+    echo "  interleave [side1_filename] [side2_filename] [output_filename]"
+    echo "  pagecount [filename]"
+}
+
+log() {
+    echo "[+] $1" 1>&2
+}
+
+error() {
+    echo "[-] $1" 1>&2
+    exit 1
+}
+
 rotate() {
     if [ 90 -eq "$1" ]; then
         angle=east
@@ -9,15 +25,14 @@ rotate() {
     elif [ 360 -eq "$1" ]; then
         angle=north
     else
-        echo "[-] invalid angle specified $1"
-        exit 1
+        error "invalid angle specified $1"
     fi
-    echo "[+] rotate $2 by $1 degrees -> $3"
+    log "rotate $2 by $1 degrees -> $3"
     pdftk "$2" cat 1-end$angle output "$3"
 }
 
 interleave() {
-    echo "[+] interleaving $1 and $2 -> $3"
+    log "interleaving $1 and $2 -> $3"
     pdftk A="$1"  B="$2"  shuffle A Bend-1 output "$3"
 }
 
@@ -46,10 +61,11 @@ case $subcommand in
         pagecount "$doc" 
         ;;
     *) 
-        echo "[-] invalid command line option"
+        log "invalid command line option"
+        show_usage
         exit 1
         ;;
 esac
 
-echo "[+] done"
+log "done"
 exit 0
